094bc2b44a49cea7b4be2f39b016da35612934e7
SaberSams <Stephen.sams@live.com>
2022-06-23 14:59:39 -0700

    Added get pending reimbursements

--- src/main/java/com/revature/service/ReimbursementService.java
+++ src/main/java/com/revature/service/ReimbursementService.java
@@ -24,5 +24,20 @@ public class ReimbursementService {
     }
     return reimbursements;
   }
+
+  public List<Reimbursement> getPendingReimbursements() {
+    List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
+    List<Employee> employees = ed.selectAll();
+    for(Employee employee: employees) {
+      for(Reimbursement reimbursement: employee.getReimbursements()) {
+        if (reimbursement.getStatus().equals(ReimbursementStatus.PENDING)) {
+          reimbursement.setAuthor(employee);
+          reimbursements.add(reimbursement);
+        }
+      }
+    }
+    return reimbursements;
+  }
+  
   
 }
