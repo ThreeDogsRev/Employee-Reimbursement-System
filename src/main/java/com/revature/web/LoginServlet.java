043a38f6267500fe52d90d19e156aadfaa8a5e39
SaberSams <Stephen.sams@live.com>
2022-06-28 13:36:02 -0700

    Login Works

---
+++ src/main/java/com/revature/web/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.revature.web;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revature.dao.Dao;
+import com.revature.dao.FakeDao;
+import com.revature.exceptions.PasswordInvalidException;
+import com.revature.exceptions.UserNotFoundException;
+import com.revature.models.Employee;
+import com.revature.service.EmployeeService;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  EmployeeService employeeService = new EmployeeService(new FakeDao());
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    Employee user = null;
+
+    try {
+      user = employeeService.confirmLogin(username, password);
+      request.getSession().setAttribute("user", user);
+    } catch (UserNotFoundException e) {
+      request.setAttribute("message", "User not found");
+    } catch (PasswordInvalidException e) {
+      request.setAttribute("message", "Password Invalid");
+    } catch (Exception e) {
+      request.setAttribute("message", e.getMessage());
+    }
+    RequestDispatcher dispatcher = (user == null)
+    ? request.getRequestDispatcher("login.html")
+    : request.getRequestDispatcher("employeeHomePage.html");
+    try{
+      dispatcher.forward(request, response);
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+
+
+  }
+
+}
