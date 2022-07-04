9115fc666c20eda1e4e6da04213c7c14f908016b
Timothy Bautista <yule2000@live.com>
2022-07-03 23:13:50 -0600

    Clean up and format service layer code

--- src/main/java/com/revature/service/ReimbursementService.java
+++ src/main/java/com/revature/service/ReimbursementService.java
@@ -7,48 +7,49 @@ import java.util.Optional;
 
 import com.revature.dao.IDao;
 import com.revature.models.Employee;
-import com.revature.models.Reimbursement;;
+import com.revature.models.Reimbursement;
 
 public class ReimbursementService {
-  private IDao<Employee> dao;
 
-  public ReimbursementService(IDao<Employee> ed) {
-    this.dao = ed;
-  }
+    private IDao<Employee> dao;
 
-  public Reimbursement getReimbursement(int id) throws SQLException {
-    Optional<Reimbursement> possibleReimbursment = getReimbursements().stream()
-        .filter(reimb -> reimb.getId() == id).findFirst();
-
-    if (possibleReimbursment.isPresent()) {
-      return possibleReimbursment.get();
+    public ReimbursementService(IDao<Employee> ed) {
+        this.dao = ed;
     }
-    throw new SQLException("No reimbursement with id " + id + " found");
-  }
 
-  public List<Reimbursement> getReimbursementsCopy() {
-    List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
-    List<Employee> employees = dao.selectAll();
-    for(Employee employee: employees) {
-      for(Reimbursement reimbursement: employee.getReimbursements()) {
-        Employee e = new Employee(employee);
-        Reimbursement r = new Reimbursement(reimbursement);
-        e.setReimbursements(null);
-        r.setAuthor(e);
-        reimbursements.add(r);
-      }
-    }
-    return reimbursements;
-  }
+    public Reimbursement getReimbursement(int id) throws SQLException {
+        Optional<Reimbursement> possibleReimbursment = getReimbursements().stream()
+            .filter(reimb -> reimb.getId() == id)
+            .findFirst();
 
-  public List<Reimbursement> getReimbursements() {
-    List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
-    List<Employee> employees = dao.selectAll();
-    for(Employee employee: employees) {
-      for(Reimbursement reimbursement: employee.getReimbursements()) {
-        reimbursements.add(reimbursement);
-      }
+        if (possibleReimbursment.isPresent()) {
+            return possibleReimbursment.get();
+        }
+        throw new SQLException("No reimbursement with id " + id + " found");
     }
-    return reimbursements;
-  }
+
+    public List<Reimbursement> getReimbursementsCopy() {
+        List<Reimbursement> reimbursements = new ArrayList<>();
+        List<Employee> employees = this.dao.selectAll();
+        for (Employee employee : employees) {
+            for (Reimbursement reimbursement : employee.getReimbursements()) {
+                Employee e = new Employee(employee);
+                Reimbursement r = new Reimbursement(reimbursement);
+                e.setReimbursements(null);
+                r.setAuthor(e);
+                reimbursements.add(r);
+            }
+        }
+        return reimbursements;
+    }
+
+    public List<Reimbursement> getReimbursements() {
+        List<Reimbursement> reimbursements = new ArrayList<>();
+        List<Employee> employees = this.dao.selectAll();
+        for (Employee employee : employees) {
+            reimbursements.addAll(employee.getReimbursements());
+        }
+        return reimbursements;
+    }
+
 }
